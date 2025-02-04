175f21f5412fb5dd359a8c49b46ecdc23ed38595
AdrianSA <50012235+cloner69@users.noreply.github.com>
2025-02-04 10:55:49 +0700

    theme change

---
+++ ThemeChanger/ThemeChanger/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  ThemeChanger
+//
+//  Created by Adrian Suryo Abiyoga on 23/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    /// View Properties
+    @State private var changeTheme: Bool = false
+    @Environment(\.colorScheme) private var scheme
+    @AppStorage("userTheme") private var userTheme: Theme = .systemDefault
+    var body: some View {
+        NavigationStack {
+            List {
+                Section("Appearance") {
+                    Button("Change Theme") {
+                        changeTheme.toggle()
+                    }
+                }
+            }
+            .navigationTitle("Settings")
+        }
+        .preferredColorScheme(userTheme.colorScheme)
+        .sheet(isPresented: $changeTheme, content: {
+            ThemeChangeView(scheme: scheme)
+                /// Since Max Height is 410
+                .presentationDetents([.height(410)])
+                .presentationBackground(.clear)
+        })
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
---
+++ ThemeChanger/ThemeChanger/ThemeChangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThemeChangerApp.swift
+//  ThemeChanger
+//
+//  Created by Adrian Suryo Abiyoga on 23/01/25.
+//
+
+import SwiftUI
+
+@main
+struct ThemeChangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
